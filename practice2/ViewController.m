f65b79e01f779e723f19ca9e662e46ce140d3329
moltak <moltak@gmail.com>
2015-06-19 18:41:17 +0900

    Initial Commit

---
+++ practice2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  practice2
+//
+//  Created by 엔갱 on 2015. 6. 19..
+//  Copyright (c) 2015년 moltak. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ practice2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  practice2
+//
+//  Created by 엔갱 on 2015. 6. 19..
+//  Copyright (c) 2015년 moltak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
